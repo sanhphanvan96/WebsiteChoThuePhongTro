e9d26bf2795437dc8584ff6788bce8c90c6d3cba
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-11-05 03:27:26 +0700

    Devise: migration for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "static#home"
 
-  get "static/home"
-  get "static/about"
+  get "home", to: "static#home"
+  get "about", to: "static#about"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
