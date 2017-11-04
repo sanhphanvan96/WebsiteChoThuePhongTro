b0e92d3512c08d5478221f8a447a4fc0c1933e6d
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-11-05 03:14:06 +0700

    Fix font awsome, precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+# Rails.application.config.assets.precompile += %w( font-awesome.min.css )
 
