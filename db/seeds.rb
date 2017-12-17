09999b7227ae7bf16c07b452e8e6cf2be32464ac
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-12-17 10:08:03 +0700

    Create seeds file for initial database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "Tôi là Admin",
+  email: "admin@admin.com",
+  password: "123456",
+  password_confirmation: "123456",
+  is_admin: true)
+
+User.create!(
+  name: "Tôi là User",
+  email: "user@user.com",
+  password: "123456",
+  password_confirmation: "123456")
+
+PostCategory.create!(
+  name: "Nhà Nguyên Căn",
+  description: "Nhà Nguyên Căn")
+
+ToiletType.create!([
+  {
+    name: "Chung"
+  },
+  {
+    name: "Khép kín"
+  }])}
