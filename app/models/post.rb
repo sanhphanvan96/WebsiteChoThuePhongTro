bbe29d07760cb9b370fe87dc07df1ca599e492ec
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-12-17 09:51:59 +0700

    Validate and limmit size for image

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   belongs_to :toilet_type
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :image, presence: true, file_size: { less_than: 3.megabytes }
 end
