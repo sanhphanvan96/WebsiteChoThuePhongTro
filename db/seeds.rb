eb1766fa81ab418ae28c8bafaff30d2946e6a8bb
sanhphanvan96 <sanhphanvan96@gmail.com>
2017-12-17 15:43:55 +0700

    Fix seeds file: add logs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,46 @@
-User.create!(
+puts "Deleting database ..."
+User.all.map(&:destroy)
+PostCategory.all.map(&:destroy)
+ToiletType.all.map(&:destroy)
+puts "Deleted database"
+
+admin = User.create!(
   name: "Tôi là Admin",
   email: "admin@admin.com",
   password: "123456",
   password_confirmation: "123456",
   is_admin: true)
+puts "Created admin: #{admin.email}, password: 123456"
 
-User.create!(
+user = User.create!(
   name: "Tôi là User",
   email: "user@user.com",
   password: "123456",
   password_confirmation: "123456")
+puts "Created user: #{user.email}, password: 123456"
 
-PostCategory.create!([
+post_category = PostCategory.create!([
   {
+    id: 1,
     name: "Nhà Nguyên Căn",
     description: "Nhà Nguyên Căn"
   },
   {
+    id: 2,
     name: "Phòng trọ",
     description: "Phòng trọ"
   }])
+puts "Created PostCategory: #{post_category[0].id}, #{post_category[0].name}"
+puts "Created PostCategory: #{post_category[1].id}, #{post_category[1].name}"
 
-ToiletType.create!([
+toilet_type = ToiletType.create!([
   {
+    id: 1,
     name: "Chung"
   },
   {
+    id: 2,
     name: "Khép kín"
   }])
+puts "Created ToiletType: #{toilet_type[0].id}, #{toilet_type[0].name}"
+puts "Created ToiletType: #{toilet_type[1].id}, #{toilet_type[1].name}"
